8e4dae3d9e4505296ddd58246f8eb4597ba2f4fd
AmaroMOO <amaroromerocedrez@gmail.com>
2024-05-22 12:44:05 +0100

    tarea.py cambiado

--- hgt/tarea.py
+++ hgt/tarea.py
@@ -1,3 +1,20 @@
 class Tarea:
-    def __init__(self) -> None:
-       pass
+    def __init__(self, tarea:str, id:int, estado:bool) -> None:
+       self.tarea = tarea
+       self.id = id
+       self.estado = estado
+
+    #Metodos CRUD
+    def read(self)->str:
+
+        return self.tarea, self.id, self.estado
+    
+    def update(self, tarea, id, estado)->None:
+        self.tarea = tarea
+        self.id = id
+        self.estado = estado
+
+    def delete(self):
+        self.tarea = None
+        self.id = None
+        self.estado = None
