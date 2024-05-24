5be367aa0642eed1562f892db142cb3560d6d153
AmaroMOO <amaroromerocedrez@gmail.com>
2024-05-24 13:00:06 +0100

    test_tarea modificado para empezarlo

--- tests/hgt/test_tarea.py
+++ tests/hgt/test_tarea.py
@@ -1,3 +1,14 @@
 import pytest
-from tarea import Tarea
+from hgt.tarea import Tarea
 
+def tarea():
+    pass
+
+def test_read(tarea):
+    pass
+
+def test_update(tarea):
+    pass
+
+def test_delete(tarea):
+    pass
